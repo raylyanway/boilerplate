0f74eeabef2e0c7dea7b5e9f312382eee43a62c2
iLya Raylyan <IRaylyan@alfabank.ru>
2021-02-27 23:43:35 +0300

    feat(*): add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./app/index.js",
+  module: {
+    rules: [
+      { test: /\.svg$/, use: "svg-inline-loader" },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      { test: /\.js$/, use: "babel-loader" },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index_bundle.js",
+  },
+};
