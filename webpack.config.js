5bc86dfa525e31569f8d54f86e49d3799d81314d
iLya Raylyan <IRaylyan@alfabank.ru>
2021-02-28 00:02:00 +0300

    feat(*): add plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./app/index.js",
@@ -16,4 +17,6 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "index_bundle.js",
   },
+  plugins: [new HtmlWebpackPlugin()],
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
 };
